1c072d9b4dceddc4e7bdda9268af9c05a2c94a7d
fumagallilaura <la_12_msn@hotmail.com>
2022-08-20 20:58:01 -0300

    feat: create Matches class model

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,32 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
+
+class Matches extends Model {
+  public homeTeam: number;
+
+  public homeTeamGoals: number;
+
+  public awayTeam: number;
+
+  public awayTeamGoals: number;
+
+  public inProgress: boolean;
+}
+Matches.init({
+  homeTeam: DataTypes.NUMBER,
+  homeTeamGoals: DataTypes.NUMBER,
+  awayTeam: DataTypes.NUMBER,
+  awayTeamGoals: DataTypes.NUMBER,
+  inProgress: DataTypes.BOOLEAN,
+}, {
+  sequelize: db,
+  modelName: 'Matches',
+  underscored: true,
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', targetKey: 'id', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', targetKey: 'id', as: 'teamAway' });
+
+export default Matches;
