53913a85350b0857b3631b728d6b5905f818121d
fumagallilaura <la_12_msn@hotmail.com>
2022-08-20 20:54:34 -0300

    feat: create Users class model

---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,25 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  public username: string;
+
+  public role: string;
+
+  public email: string;
+
+  public password: string;
+}
+Users.init({
+  username: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  sequelize: db,
+  modelName: 'Users',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Users;
