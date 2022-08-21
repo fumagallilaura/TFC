c5a11ebbdbd5d99746a9a66fea1c317953dce680
fumagallilaura <la_12_msn@hotmail.com>
2022-08-21 00:50:00 -0300

    feat: add app.use routes login

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import routes from './routes';
 
 class App {
   public app: express.Express;
@@ -7,12 +8,12 @@ class App {
     this.app = express();
 
     this.config();
-
+    this.app.use(routes);
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
   }
 
-  private config():void {
+  private config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
@@ -24,7 +25,7 @@ class App {
     this.app.use(accessControl);
   }
 
-  public start(PORT: string | number):void {
+  public start(PORT: string | number): void {
     this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
   }
 }
