e6a1a9fd21d91d8b3e0e73efd0e8e474ba217686
fumagallilaura <la_12_msn@hotmail.com>
2022-08-21 00:46:10 -0300

    feat: create service user class

---
+++ app/backend/src/services/User.ts
@@ -0,0 +1,15 @@
+import * as bcrypt from 'bcryptjs';
+import User from '../database/models/Users';
+
+export default class UserService {
+  public static async login(email: string, password: string) {
+    const user = await User.findOne({ where: { email } });
+    if (!user) return null;
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) return null;
+
+    const { id, username, role } = user;
+    return { id, username, role, email };
+  }
+}
