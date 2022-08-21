0a703cfa75b330d5e26680799c15a1bf34d38040
fumagallilaura <la_12_msn@hotmail.com>
2022-08-20 21:07:57 -0300

    feat: create matches migrations

--- app/backend/src/database/migrations/20220821000319-create-matches.js
+++ app/backend/src/database/migrations/20220821000319-create-matches.js
@@ -1,21 +1,39 @@
 'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          field: 'id'
+        },
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          field: 'id'
+        },
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER
+      },
+      in_progress: {
+        type: Sequelize.BOOLEAN
+      },
+    });
   },
-
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('matches');
   }
 };
