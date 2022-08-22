b3c8bf37b838cba8855ce6596df234a4843f79bf
fumagallilaura <la_12_msn@hotmail.com>
2022-08-22 18:49:29 -0300

    feat: add get team by id service

--- app/backend/src/services/Team.ts
+++ app/backend/src/services/Team.ts
@@ -4,4 +4,9 @@ export default class TeamService {
   public static async getAll() {
     return Team.findAll();
   }
+  public static async getById(id: string) {
+    const team = await Team.findByPk(id);
+    if (!team) return null;
+    return team;
+  }
 }
