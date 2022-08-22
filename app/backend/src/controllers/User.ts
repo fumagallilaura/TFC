06532c6607dfc9ce6dc0b91f7a16d008b5c13bbf
fumagallilaura <la_12_msn@hotmail.com>
2022-08-22 16:43:24 -0300

    feat: implements validate method on controller

--- app/backend/src/controllers/User.ts
+++ app/backend/src/controllers/User.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
 import * as services from '../services';
 import * as jwt from 'jsonwebtoken';
+import Users from '../database/models/Users';
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export default class UsersController {
-  static async login(req: Request, res: Response) {
+  public static async login(req: Request, res: Response) {
     const { email, password } = req.body;
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
@@ -18,5 +19,20 @@ export default class UsersController {
 
     const token = jwt.sign(email, JWT_SECRET);
     res.status(200).json({ user, token });
+  }
+
+  public static async validate(req: Request, res: Response) {
+    const { authorization } = req.headers;
+
+    if (!authorization) return res.status(401).json({
+      message: 'You must have a authorization'
+    })
+    const email = jwt.verify(authorization, JWT_SECRET)
+    const user = await Users.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'User dont exist' });
+    }
+    res.status(200).json(user.role);
   }
 }
