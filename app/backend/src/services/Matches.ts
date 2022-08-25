fa570186374598eab7320e6f7239d24b30948951
fumagallilaura <la_12_msn@hotmail.com>
2022-08-25 18:31:58 -0300

    refactor: matches service

--- app/backend/src/services/Matches.ts
+++ app/backend/src/services/Matches.ts
@@ -6,12 +6,20 @@ import Teams from '../database/models/Teams';
 import Matches from '../database/models/Matches';
 
 export default class MatchService {
-  public static async getAll() {
+  public static async getAll(inProgress?: string) {
     // https://github.com/sequelize/sequelize-typescript/tree/1.0.0#upgrade-to-sequelize-typescript1
     let findOptions:FindOptions = { include: [
       { model: Teams, as: 'teamHome', attributes: { exclude: ['id'] } },
       { model: Teams, as: 'teamAway', attributes: { exclude: ['id'] } },
     ]};
+    if (inProgress === 'true') {
+      findOptions = {
+        where: { inProgress: true }, ...findOptions };
+    }
+    if (inProgress === 'false') {
+      findOptions = {
+        where: { inProgress: false }, ...findOptions };
+    }
     const matches = await Matches.findAll(findOptions);
     return matches;
   }
