4f0a375e462a566d4d77a73dce4f5cce9e997012
fumagallilaura <la_12_msn@hotmail.com>
2022-08-27 00:06:47 -0300

    feat: add post route matches

--- app/backend/src/routes/matches.ts
+++ app/backend/src/routes/matches.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import Autentication from '../middlewares/Autentication';
 import Matches from '../controllers/Matches';
 const router = Router();
+const auth = new Autentication();
 
 router.get('/', Matches.getAll);
+router.post('/', auth.middleware, Matches.teamsValidation, Matches.create);
 
 export default router;
