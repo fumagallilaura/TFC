8af40ed05d578e898c37b30b18395c58f1545c6f
fumagallilaura <la_12_msn@hotmail.com>
2022-08-29 23:12:25 -0300

    feat: finish and update route added

--- app/backend/src/routes/matches.ts
+++ app/backend/src/routes/matches.ts
@@ -5,5 +5,7 @@ const router = Router();
 
 router.get('/', Matches.getAll);
 router.post('/', auth, Matches.teamsValidation, Matches.create);
+router.patch('/:id/finish', auth, Matches.finish);
+router.patch('/:id', auth, Matches.update);
 
 export default router;
